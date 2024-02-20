25905bb76a85071442992a5719d01b18cac31323
Rafael Borges <rafael.borges2698@gmail.com>
2024-02-20 01:02:41 -0300

    feat/SearchForm: add component SearchForm to page Transactions

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,5 +1,6 @@
 import { Header } from "../../components/Header";
 import { Summary } from "../../components/Summary";
+import { SearchForm } from "./Components/SearchForm";
 import { PriceHighLight, TransactionContainer, TransactionsTable } from "./styles";
 
 export function Transactions() {
@@ -9,6 +10,8 @@ export function Transactions() {
             <Summary/>
 
             <TransactionContainer>
+            <SearchForm />
+            
             <TransactionsTable>
                 <tbody>
                     <tr>
--- src/pages/Transactions/styles.ts
+++ src/pages/Transactions/styles.ts
@@ -12,7 +12,7 @@ export const TransactionsTable = styled.table`
     width: 100%;
     border-collapse: separate;
     border-spacing: 0 0.5rem; //separar as bordas da tabela
-    //margin-top: 1.5rem;
+    margin-top: 1.5rem;
 
     td {
         padding: 1.25rem 2rem;
